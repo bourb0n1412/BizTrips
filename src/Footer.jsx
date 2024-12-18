dea645f1ff0f86358f2523f790a401810d4522d8
lcavuoti <lcavuoti@clip.ch>
2024-12-18 10:51:46 +0100

    footer 2025

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         This site is created for demonstrative purposes only and does not offer
         any real products or services.
       </p>
-      <p>&copy; Happy New Year, BBW 2024</p>
+      <p>&copy; Happy New Year, BBW 2025</p>
     </footer>
   );
 }
